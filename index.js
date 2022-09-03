940511f02fa7d3e61f020aed1349654b0a0e067b
ctYurk15 <levgenetic@gmail.com>
2022-09-03 17:59:47 +0300

    Add borders

--- index.js
+++ index.js
@@ -3,3 +3,50 @@ const c = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+class Boundary
+{
+
+    static width = 40;
+    static height = 40;
+
+    constructor({position})
+    {
+        this.position = position;
+        this.width = 40;
+        this.height = 40;
+    }
+
+    draw()
+    {
+        c.fillStyle = 'blue';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const map = [
+    ['-', '-', '-', '-', '-', '-', ],
+    ['-', ' ', ' ', ' ', ' ', '-', ],
+    ['-', ' ', '-', '-', ' ', '-', ],
+    ['-', ' ', ' ', ' ', ' ', '-', ],
+    ['-', '-', '-', '-', '-', '-', ],
+];
+
+const boundaries = [];
+
+map.forEach(function(row, i){
+    row.forEach(function(symbol, j){
+        
+        switch(symbol)
+        {
+            case '-':
+                boundaries.push(new Boundary({position: {x: Boundary.width * j, y: Boundary.height * i}}))
+                break;
+        }
+
+    });
+});
+
+boundaries.forEach(function(boundary){
+    boundary.draw();
+});
